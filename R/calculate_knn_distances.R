cc0a4969961d1b4d10d67bdf8d0dc9054c59e183
erik <erik.doffagne@gmail.com>
2017-11-21 14:53:25 +0100

    Passed additional parameters to the Python code

--- R/calculate_knn_distances.R
+++ R/calculate_knn_distances.R
@@ -1,10 +1,11 @@
 calculate_knn_distances = function(template_features_sparse_clean=data,
                                    perplexity=perplexity, mem_usage=0.9,
-                                   verbose=True)
+                                   verbose=TRUE)
 {
   file = system.file("python", "gpu.py", package="cuda.tsne")
   dir = gsub("gpu.py", "", file)
   mod = import_from_path("gpu", dir)
-  res = mod$calculate_knn_distances(template_features_sparse_clean, perplexity)
+  res = mod$calculate_knn_distances(template_features_sparse_clean, mem_usage=mem_usage,
+                                    perplexity=perplexity, verbose=verbose)
   return(res)
 }
