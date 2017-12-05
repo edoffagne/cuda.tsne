022e265570b39befe23a47f4fc68231749c0bc03
erik <erik.doffagne@gmail.com>
2017-12-05 08:27:03 +0100

    Added tests for calculate_knn_distances

---
+++ tests/testthat/test-calculate_knn_distances.R
@@ -0,0 +1,16 @@
+source("utils.R")
+
+test_that('calculate_knn_distances returns expected results', {
+  skip_if_no_accelerate()
+  set.seed(123)
+  iris_unique = unique(iris) # Remove duplicates
+  iris_matrix = as.matrix(iris_unique[,1:4])
+  d = cuda.tsne:::calculate_knn_distances(template_features_sparse_clean=iris_matrix,
+                            perplexity=30, mem_usage=0.9,
+                            verbose=FALSE)
+
+  expect_equal(length(d), 3)
+
+})
+
+
